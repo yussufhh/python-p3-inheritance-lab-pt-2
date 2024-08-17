314250e47778540a2e4adbc8dbc5b83b040a39c2
yussufhh <0793477632g@gmail.com>
2024-08-17 08:01:42 +0300

    final-commit

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,17 @@
 class Student:
-    pass
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
+    
+    def raise_hand(self):
+        print("Pick me!")
 
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        super().hello()  # Call the hello() method from the Student class
+        print("How are you doing today? I'm okay, but I'm kind of tired. "
+              "Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! "
+              "What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+        for _ in range(10):
+            super().raise_hand()  # Call the raise_hand() method from the Student class 10 times
